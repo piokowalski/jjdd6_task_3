4c9c22beb9cf53fca538513322eea08ed05f0adc
piokowalski <piokowalski@onet.pl>
2019-03-25 16:34:13 +0100

    Initial commit - exercises 1 - loops done

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,41 @@ public class Main {
 
         for (int i = 0; i < howManyNames; i++) {
             System.out.println("Enter the name below: ");
-
+            scanner = new Scanner(System.in);
+            // scanner needs to be repeated (scanners issue)
             names[i] = scanner.nextLine();
         }
 
+        String longest = names[0];
+        for (int i=0; i <howManyNames; i++) {
+            if (names[i].length() > longest.length()) {
+                longest = names[i];
+            }
+        }
+        System.out.println("The longest name is :" + longest);
+
+
+        String shortest = names[0];
+        for (int i=0; i <howManyNames; i++) {
+            if (names[i].length() < shortest.length()) {
+                shortest = names[i];
+            }
+        }
+        System.out.println("The shortest name is: " + shortest);
 
 
 
+
+
+        /**
+         * longest = names[0]
+         * for (0....howManyNames) {
+         *   if (names[i].length() > longest) {
+         *     longest = names[i]
+         *   }
+         * }
+         *
+         * ->>> max to longest
+         */
     }
-
 }
