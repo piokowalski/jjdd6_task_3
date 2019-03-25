5b136ac75e7caff464fe25fc48aa1547c80ac785
piokowalski <piokowalski@onet.pl>
2019-03-25 15:40:48 +0100

    Initial commit - exercises 1 - loop and scanners created

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int howManyNames;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how many names below:");
+        howManyNames = scanner.nextInt();
+
+        String[] names = new String[howManyNames];
+
+        for (int i = 0; i < howManyNames; i++) {
+            System.out.println("Enter the name below: ");
+
+            names[i] = scanner.nextLine();
+        }
+
+
 
 
     }
+
 }
