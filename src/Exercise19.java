16f7fa4004ab7a1b28632d2a36f53af9ff7775a5
piokowalski <piokowalski@onet.pl>
2019-03-26 10:08:02 +0100

    Initial commit - exercises 19 from JSE3 - arrays

---
+++ src/Exercise19.java
@@ -0,0 +1,29 @@
+public class Exercise19 {
+
+    //JSE3 arrays exercise - 2019.03.26
+
+    public static void main(String[] args) {
+
+        printingArray(4);
+
+    }
+
+
+
+        public static void printingArray(int param) {
+            int[] arr = new arr[10];
+
+            for (int a = 0; a < arr.length; a++) {
+
+                arr[a] = param + a;
+            }
+            for (int a = 0; a < arr.length; a++) {
+                System.out.println(arr[a]);
+            }
+        }
+
+
+
+    }
+
+
